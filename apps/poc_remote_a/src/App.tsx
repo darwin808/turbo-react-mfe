7e331e64e9f78223b53d0eadd409a84336bde256
darwin.apolinario <darwin.apolinario@gmail.com>
2024-06-20 15:26:03 +0800

    fix: undefind usetate in remote

--- apps/poc_remote_a/src/App.tsx
+++ apps/poc_remote_a/src/App.tsx
@@ -11,9 +11,9 @@ function App() {
   return (
     <div className="bg-green-400 p-10">
       REMOTE A
-      <Card className='rounded-lg border-sky-50'>
+      <Card className="rounded-lg border-sky-50">
         <CardHeader>
-          <CardTitle className='text-lg'>Card Title</CardTitle>
+          <CardTitle className="text-lg">Card Title</CardTitle>
           <CardDescription>Card Description</CardDescription>
         </CardHeader>
         <CardContent>
